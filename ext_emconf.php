c76e7e5e981cc0f1dde19d651697cc3282023773
René Nitzsche <nitzsche@dmk-ebusiness.de>
2019-11-24 20:16:46 +0100

    enable T3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'DFL Sync',
 	'description' => 'Sync t3sports competitions with DFL data',
 	'category' => 'backend',
-	'version' => '0.2.1',
+	'version' => '0.2.2',
 	'state' => 'beta',
 	'uploadfolder' => false,
 	'createDirs' => '',
@@ -23,20 +23,15 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_email' => 'rene@system25.de',
 	'author_company' => 'System 25',
 	'constraints' =>
-	array (
+	[
 		'depends' =>
-		array (
-			'typo3' => '4.5.0-6.2.99',
-			'rn_base' => '0.14.1-0.0.0',
-			'cfc_league' => '1.0.0-0.0.0',
-			'scheduler' => '6.2.0-0.0.0',
-		),
-		'conflicts' =>
-		array (
-		),
-		'suggests' =>
-		array (
-		),
-	),
-);
+		[
+			'typo3' => '4.5.0-8.7.99',
+			'rn_base' => '1.10.1-0.0.0',
+			'cfc_league' => '1.3.0-0.0.0'
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
 
